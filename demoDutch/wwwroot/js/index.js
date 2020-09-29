499bedc71141ed1f5f5d6526ca88effde563fcd9
knowledge <knowledge@tshimologong.joburg>
2020-09-29 13:29:02 +0200

    aading java css

---
+++ demoDutch/wwwroot/js/index.js
@@ -0,0 +1,9 @@
+﻿var theForm = document.getElementById("theForm");
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function (){
+    alert("gghS");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
+var listitem = productInfo.item[0].children;
